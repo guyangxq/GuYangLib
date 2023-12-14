c0ffbacb0aad26e9a70ada9e6bcb05416eeed706
GuYang <545950859@qq.com>
2023-12-14 18:15:00 +0800

    123123

--- TestLib/Program.cs
+++ TestLib/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("hello Testlib");
 
 var utils=new Utils();
 Utils.Info();
+var Solution=new Solution();
+// Solution.TwoSum
+Console.WriteLine(Solution.GetGcd(60,240));
--- src/leetcode/Solution.cs
+++ src/leetcode/Solution.cs
@@ -1,5 +1,11 @@
 public class Solution
 {
+    /// <summary>
+    /// 给定一个整数数组 nums 和一个整数目标值 target，在该数组中找出和为目标值 target的那两个整数
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns>返回它们的数组下标</returns>
     public int[] TwoSum(int[] nums, int target) {
         foreach(var item in nums)
         {
@@ -21,5 +27,30 @@ public class Solution
             }
         }
         return new int[]{};
+    }
+    /// <summary>
+    /// 求m和n的最大公约数
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="n"></param>
+    /// <returns>如果返回0，说明没有最大公约数</returns>
+    public int GetGcd(int m,int n)
+    {
+        if(n==0)return m;
+        int[] result=ComputeGcd(m,n);
+        m=result[0];
+        n=result[1];
+        while(n!=0)
+        {
+            result=ComputeGcd(m,n);
+            m=result[0];
+            n=result[1];
+        }
+        return m;
+    }
+    //公约数一次计算
+    private int[] ComputeGcd(int m,int n)
+    {
+        return new int[]{n,m%n};
     }
 }
